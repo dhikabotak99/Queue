dc86425e77515e081a197a6dfe95b0edcb8274c0
dhikabotak99 <wicakbotak99@gmail.com>
2019-09-14 23:39:16 +0700

    Make Object in Queue Main Class

--- src/QueueMain.java
+++ src/QueueMain.java
@@ -3,5 +3,9 @@
  * @author Etem
  */
 public class QueueMain {
-    
+    public static void main(String[] args){
+        //Membuat Object Queue
+        Queue q = new Queue(10);
+        
+    }
 }
